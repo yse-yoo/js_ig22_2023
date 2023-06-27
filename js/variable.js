92c6286d59a260112c43b7c9f21dc8699913ed3a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-27 10:06:30 +0900

    update

---
+++ js/variable.js
@@ -0,0 +1,15 @@
+// 変数（variable）の定義と値の代入（だいにゅう）
+var monster = "スライム"
+
+// number
+var amount = 2
+var price = 1000
+var discount = -100
+
+var average = 0.333
+var speed = 45.5
+speed = 55.3
+
+// boolean
+var isActive = true
+isActive = false

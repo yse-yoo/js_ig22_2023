ab22718c51c57ba01d9a50e8325ef9bcc1d4587d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-27 10:35:14 +0900

    update

--- js/calculate.js
+++ js/calculate.js
@@ -2,6 +2,7 @@ var hp;
 
 hp = 4
 
+// 2項演算
 hp = hp + 5
 console.log(hp)
 
@@ -14,3 +15,29 @@ console.log(hp)
 hp = hp / 3
 console.log(hp)
 
+// 単項演算
+console.log("--- unary operation ---")
+hp = 5
+hp++
+console.log(hp)
+
+hp = 5
+hp--
+console.log(hp)
+
+// 複合演算
+console.log("--- compound operation ---")
+hp = 5
+hp += 3
+console.log(hp)
+
+hp = 5
+hp -= 3
+console.log(hp)
+
+// 文字と連結（れんけつ）
+hp = 10
+monster_name = "スライム"
+
+var message = monster_name + "があらわれた！"
+console.log(message)

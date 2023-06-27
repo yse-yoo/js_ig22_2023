7fa4dc6e68d2f066629084a46c1e246ae45d2ca8
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-27 11:53:07 +0900

    add function

--- js/function.js
+++ js/function.js
@@ -1,4 +1,7 @@
-
 function calculateTotalPrice(price, amount) {
-    
+    var result = price * amount;
+    return result;
 }
+
+var totalPrice = calculateTotalPrice(200, 5);
+console.log(totalPrice);

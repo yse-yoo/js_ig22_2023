b71951d20abe28218c3bc3a9faec1e0b07d0a5e3
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-27 09:56:24 +0900

    add commnets

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
+// Ctrl + / でコメント(Windows)
+// Cmd + / でコメント（Mac）
+// コメントです
 console.log("Hello, Yoshikawa...")
+// console.log("今日は曇りです")
+
+/**
+ * note 1
+ * note 2
+ */
+
+// Formatterの Short cut key
+// Shift + Alt + F (Windows)
+// Shift + Option + F (Windows)

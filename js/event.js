c0edc3e21a7ab3056f8dae245aa56424ee3d570a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-04 11:29:32 +0900

    update

--- js/event.js
+++ js/event.js
@@ -1,3 +1,7 @@
 function inputUserName() {
    console.log("change!!") 
+   // user_name 取得（しゅとく）
 }
+
+var userNameElement = document.getElementById('user_name')
+var messageElement = document.getElementById('message')

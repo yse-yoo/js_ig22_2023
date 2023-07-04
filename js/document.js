1c886b4d3d1296fb85fb0415dc6012d1f5a09b7d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-04 09:35:10 +0900

    update

--- js/document.js
+++ js/document.js
@@ -1,3 +1,18 @@
+/**
+ * Pタグをアウトプットするメソッド
+ * anonymous function
+ */
+const outputP = function(message) {
+    var tag = "<p>" + message + "</p>"
+    document.write(tag)
+}
+
+
 document.write("<h1>Profile</h1>")
 document.write("<h2>Name</h2>")
 document.write("<p>YSE</p>")
+document.write("<h2>Favorite</h2>")
+
+outputP("旅行")
+outputP("ゲーム")
+outputP("スポーツ")

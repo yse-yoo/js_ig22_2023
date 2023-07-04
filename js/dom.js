ddcf76c65c39d1e67d8cbb5e486ee2356b107493
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-04 10:26:45 +0900

    update

--- js/dom.js
+++ js/dom.js
@@ -5,4 +5,5 @@ console.log(price)
 
 // update element by id
 var messageElement = document.getElementById("message")
-messageElement.innerText = "Welcome!!"
+// messageElement.innerText = "<p>Welcome!!</p>"
+messageElement.innerHTML = "<p>Welcome!!</p>"

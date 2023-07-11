53c92754df9d1d14afd3f6537c4655845c222b09
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-11 10:05:54 +0900

    add timer

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,16 @@
 var counterElement = document.getElementById("counter")
 var interval = 1000  //1sec
 var timer
+
+//カウンター初期化(Initialize)
 var count = 0
+counterElement.innerText = count
+
+function start() {
+    console.log("Start!!!")
+
+    timer = setInterval(() => {
+        count++    
+        counterElement.innerHTML = count
+    }, interval);
+}

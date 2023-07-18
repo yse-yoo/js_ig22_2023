6ac21ce5b418b6809154aaeb1c30bbd7cd8d3c27
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-18 12:04:11 +0900

    add lottery

---
+++ js/lottery.js
@@ -0,0 +1,7 @@
+var rarities = [
+    { type: "N", name: "Normal", probability: 70 },
+    { type: "R", name: "Rare", probability: 20 },
+    { type: "SR", name: "Super Rare", probability: 6 },
+    { type: "SSR", name: "Super Special Rare", probability: 3 },
+    { type: "LR", name: "Legend", probability: 1 },
+]

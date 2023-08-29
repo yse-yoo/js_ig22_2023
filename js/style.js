8f0901582c5a2ee88ae8156ee17a4b64db49fb07
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-29 12:01:49 +0900

    replace()

--- js/style.js
+++ js/style.js
@@ -4,3 +4,15 @@ var messageElement = document.getElementById("message")
 function add() {
     messageElement.classList.add("active")
 }
+
+function remove() {
+    messageElement.classList.remove("active")
+}
+
+function toggle() {
+    messageElement.classList.toggle("active")
+}
+
+function replace() {
+    messageElement.classList.replace("message", "frame")
+}

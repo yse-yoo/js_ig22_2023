ac1e83cc6d3f7a70b81bd97e6679bccedd89377c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-29 10:24:40 +0900

    update

--- js/callback.js
+++ js/callback.js
@@ -1,7 +1,14 @@
 function processArray(arr, callback) {
-    //繰り返し、引数 callbackを実行
+    //繰り返し
     for (const number of arr) {
+        //引数（function型） callback を実行
         callback(number)
     }
 }
 
+const numbers = [1, 2, 3, 4, 5]
+processArray(numbers, function (number) {
+    //引数を2倍する
+    const answer = number * 2
+    console.log(answer)
+})

32fb6ae44f4e12adff06a934bd7b794f2d802998
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-29 10:02:28 +0900

    update

--- js/loop.js
+++ js/loop.js
@@ -22,7 +22,15 @@ for (const key in person) {
 
 const drinks = ["コーヒー", "紅茶", "ほうじ茶"]
 //for-i
+console.log("--- for-i ---")
 for (var index = 0; index < drinks.length; index++) {
     const drink = drinks[index];
+    console.log(drink) 
+}
+
+//for-in
+console.log("--- for-in ---")
+for (const key in drinks) {
+    const drink = drinks[key];
     console.log(drink) 
 }

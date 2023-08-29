005d44bc7bf7adcd887a1723e452e0e538e57f25
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-29 10:32:00 +0900

    update

--- js/callback.js
+++ js/callback.js
@@ -7,20 +7,31 @@ function processArray(arr, callback) {
 }
 
 const numbers = [1, 2, 3, 4, 5]
+console.log("--- proccessArray() 1 ---")
 processArray(numbers, function (number) {
     //引数を2倍する
     const answer = number * 2
     console.log(answer)
 })
 
+console.log("--- proccessArray() 2 ---")
 processArray(numbers, function (number) {
     //計算
     const answer = (number + 5) * 0.5
     console.log(answer)
 })
 
-processArray(numbers, function (number) {
+console.log("--- proccessArray() 3 ---")
+processArray(numbers,  (number) => {
     //計算
     const answer = (number - 5) * 0.1
     console.log(answer)
 })
+
+console.log("--- proccessArray() 4 ---")
+function calculate(number) {
+    const answer = (number - 0.3) * 1.1
+    console.log(answer)
+}
+
+processArray(numbers, calculate)

5dd518fe3509b0719bb4c955837ef7120c41ec21
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-12 10:17:48 +0900

    update

--- js/group.js
+++ js/group.js
@@ -5,17 +5,17 @@ console.log(cities)
 console.log("--- for of ---")
 //ただしく動く
 for (const city of cities) {
-    console.log(city)
+    console.log(city.innerHTML)
 }
 
 console.log("--- for in ---")
 // HTML Collection は for in を使うとおかしくなる
 for (const index in cities) {
-    console.log(cities[index])
+    console.log(cities[index].innerHTML)
 }
 
 console.log("--- forEach ---")
 // forEach は HTML Collection から Array にして動かす
 Array.from(cities).forEach(city => {
-   console.log(city) 
+   console.log(city.innerHTML) 
 });

700b4b9eadfa116c8c1c66987af9cba298b2625e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-19 09:21:53 +0900

    update

---
+++ js/login.js
@@ -0,0 +1,10 @@
+
+function auth() {
+    var email = document.getElementsByName('email')[0].value
+    var password = document.getElementsByName('password')[0].value
+
+    console.log(email)
+    console.log(password)
+
+    return false;
+}

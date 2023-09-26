eb397e287ce16c9aa95b14c9913f2955263f401f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-26 11:19:37 +0900

    add loadPrefectures()

--- js/search_address.js
+++ js/search_address.js
@@ -2,11 +2,20 @@ function getApiURL() {
     const currentURL = location.href
     const fileName = currentURL.substring(currentURL.lastIndexOf('/') + 1)
     const baseURL = currentURL.replace(fileName, '')
-    console.log(currentURL)
-    console.log(fileName)
-
     return baseURL
 }
 
 const API_URL = getApiURL() + 'data/prefectures.json'
 console.log(API_URL)
+
+// 非同期処理で、都道府県API読み込み
+const loadPrefectures = async () => {
+    // APIに接続して、レスポンスがあるまで待つ
+    const response = await fetch(API_URL)
+    // レスポンス）（JSONデータ）を、オブジェクトに変換
+    const prefectures = await response.json()
+    console.log(prefectures)
+}
+
+// 都道府県読み込み処理
+loadPrefectures()

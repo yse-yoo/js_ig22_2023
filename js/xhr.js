fd0ef8f93b012889bdd06ffd9e2dcd58c306f3ff
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-26 09:44:50 +0900

    add xhr html

---
+++ js/xhr.js
@@ -0,0 +1,12 @@
+function getApiURL() {
+    const currentURL = location.href
+    const fileName = currentURL.substring(currentURL.lastIndexOf('/') + 1)
+    const baseURL = currentURL.replace(fileName, '')
+    console.log(currentURL)
+    console.log(fileName)
+
+    return baseURL
+}
+
+const API_URL = getApiURL() + 'data/persons.json';
+console.log(API_URL)
